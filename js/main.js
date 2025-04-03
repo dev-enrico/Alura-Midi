933b9a454aca475425bf54c7dff501a554806b91
Enrico Ferraz <pedroenricoferraz@gmail.com>
2025-04-03 19:05:24 -0300

    projeto inicial

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function playSound(audioSelector) {
+    const element = document.querySelector(audioSelector);
+
+    if (element && element.localName === 'audio') {
+            element.play();
+    } else {
+        console.log('Element or selector not found!');
+    }
+}
+
+const keyList = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < keyList.length; i++) {
+
+    const key = keyList[i];
+    const instrument = key.classList[1];
+    const idSound = `#som_${instrument}`;
+
+    key.onclick = function () {
+        playSound(idSound);
+    } 
+
+    key.onkeydown = function (event) {
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+            key.classList.add('ativa');
+    }
+    
+    key.onkeyup = function () {
+        key.classList.remove('ativa');   
+    }
+}
+}
